aa45a401593f1374428716deef53a7cc88e5b3e1
gurpreetsinghbaiwan <gurpreet.baidwan@cloudsmartz.net>
2017-01-24 13:20:11 +0530

    setenv added

---
+++ setenv.sh
@@ -0,0 +1,2 @@
+export JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=192.168.80.127"
+
